8b15cee4dfbb7be7c0d376bb48385ef6cc637088
Thiagotatagiba <thiagoctatagiba@gmail.com>
2015-05-05 19:32:29 -0300

    Thiago Tatagiba - Cadastro de atividades: Nome, Tipo, Data, Valor.

--- TrabalhoPOO/src/trabalhopoo/model/pojo/Atividade.java
+++ TrabalhoPOO/src/trabalhopoo/model/pojo/Atividade.java
@@ -11,4 +11,66 @@ package trabalhopoo.model.pojo;
  */
 public class Atividade {
     
+    private String nome;
+    private String tipo;
+    private float valor;
+    private String data;
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the valor
+     */
+    public float getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the data
+     */
+    public String getData() {
+        return data;
+    }
+
+    /**
+     * @param data the data to set
+     */
+    public void setData(String data) {
+        this.data = data;
+    }
+    
+    
 }
